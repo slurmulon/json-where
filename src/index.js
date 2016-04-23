c2832dde1ab411ced714142e28219bab5fec647c
Erik Vavro <erik@sighten.io>
2016-04-23 14:59:31 -0700

    :wrench: updated index imports (missed some abstract modules)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {AbstractRel, AbstractRelSpec}         from './abstract'
-import {PathRel,     PathRelSpec,    path}    from './path'
-import {PointerRel,  PointerRelSpec, pointer} from './pointer'
-import {QueryRel,    QueryRelSpec,   query}   from './query'
+import {AbstractRel, AbstractRelSpec, which,  $} from './abstract'
+import {PathRel,     PathRelSpec,     path}      from './path'
+import {PointerRel,  PointerRelSpec,  pointer}   from './pointer'
+import {QueryRel,    QueryRelSpec,    query}     from './query'
 
 export default {
-  AbstractRel, AbstractRelSpec, $,      which,
+  AbstractRel, AbstractRelSpec, which,  $,
   PathRel,     PathRelSpec,     path,
   PointerRel,  PointerRelSpec,  pointer,
   QueryRel,    QueryRelSpec,    query
