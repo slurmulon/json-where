83f48cf5f6eb3db4c03b376d39fba58e988d665b
Erik Vavro <erik@sighten.io>
2016-04-23 17:14:26 -0700

    :wrench: default package script. also trimmed down index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import {AbstractRel, AbstractRelSpec, which,  $} from './abstract'
-import {PathRel,     PathRelSpec,     path}      from './path'
-import {PointerRel,  PointerRelSpec,  pointer}   from './pointer'
-import {QueryRel,    QueryRelSpec,    query}     from './query'
-
-export default {
-  AbstractRel, AbstractRelSpec, which,  $,
-  PathRel,     PathRelSpec,     path,
-  PointerRel,  PointerRelSpec,  pointer,
-  QueryRel,    QueryRelSpec,    query
-}
+export {AbstractRel, AbstractRelSpec, which,  $} from './abstract'
+export {PathRel,     PathRelSpec,     path}      from './path'
+export {PointerRel,  PointerRelSpec,  pointer}   from './pointer'
+export {QueryRel,    QueryRelSpec,    query}     from './query'
